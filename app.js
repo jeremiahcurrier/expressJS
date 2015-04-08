d2c5e28423acf913657fece8895bb3b3302a1b35
Jeremiah Currier <jeremiah@frontapp.com>
2015-04-08 09:42:24 -0700

    Adding examples & comments

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 var express = require('express');
 var app = express();
 
-app.get('/cities', function (request, response) {
-  var cities = ['Caspiana', 'Indigo', 'Paradise'];
-  response.send(cities); // can also use response.json(cities)
+// app.get('/', function(request, response) {
+// 	// Line 6 is the same as lines 8 & 9
+// 	response.send('Hello World'); // Express API
+// 	// Lines 8 & 9 are the same as line 6
+// 	response.write('Hello World'); // 1/2 Node API
+// 	response.end(); // 2/2 Node API
+// });
+
+app.get('/blocks', function(request, response) {
+	// var blocks = ['Fixed', 'Movable', 'Rotating'];
+	// // response.send(blocks);
+	// response.json(blocks); // for objects & arrays this functions the same as the send() function
+
+	// // What is passing a string to send() function? 
+	// var blocks = "<ul><li>Fixed</li><li>Movable</li></ul>";
+	// response.send(blocks);
+	// // Responding with straight HTML from a route 
+	// // Server side rending look into EJS & Jade
+
+	// // What if move existing route to new location? use response.redirect() passing in destination path as an argument
+	// response.redirect('/parts');
+	
+	// What is move existin route to a new route PERMANENTLY? 
+	response.redirect(301, '/parts');
 });
 
-// Move route to existing location
-app.get('/locations', function(request, response) {
-  response.redirect(301, '/cities');
-});
-
-app.listen(3001, function () {
-  console.log("Running Express");
-});
+app.listen(3000);
